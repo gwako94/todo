fa47a3496ef6431f42e8f4ca08849d62e048c38d
gwako94 <galgallowako254@gmail.com>
2017-06-03 15:53:23 +0300

    validation

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,2 +1,14 @@
 class Task < ActiveRecord::Base
+  class Task < ActiveRecord::Base
+  has_one :title
+  validates_presence_of :title
+  validate :future_completed_date
+
+  private
+    def future_completed_date
+      if !completed.blank? && completed > Date.today
+        self.errors.add(:completed, "can't be in the future")
+      end
+    end
+  end
 end
